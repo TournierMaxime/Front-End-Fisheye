df1d117f5f5226c338980a853a3abbb65a124a9b
Hoggy06 <speedrun.fra@gmail.com>
2022-12-08 22:02:34 +0100

    Initialisation des classes

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,24 @@
 //Mettre le code JavaScript lié à la page photographer.html
+import { getPhotographers } from "./index.js";
+import { mediasFactory } from "../factories/photographerMedias.js";
+const search_params = new URLSearchParams(window.location.search);
+const photographerId = search_params.get("id");
+
+async function displayData(photographers) {
+  //const photographersHeader = document.querySelector(".photograph-header");
+  photographers.forEach((photographer) => {
+    if (photographer.id == photographerId) {
+      const photographerModel = mediasFactory(photographer);
+      photographerModel.getUserMediaCardDOM();
+      //photographersHeader.appendChild(userCardDOM);
+    }
+  });
+}
+
+async function init() {
+  // Récupère les datas des photographes
+  const { photographers } = await getPhotographers();
+  displayData(photographers);
+}
+
+init();
