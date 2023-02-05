bd4d29cd79f141f448fc349d8cfa2e8753c0cc14
Hoggy06 <speedrun.fra@gmail.com>
2023-02-05 17:19:34 +0100

    fix

--- scripts/utils/getData.js
+++ scripts/utils/getData.js
@@ -2,7 +2,7 @@
 
 // Retrieve all photographers data
 async function getPhotographers () {
-  const request = await fetch('/Front-End-Fisheye/../../data/photographers.json')
+  const request = await fetch('./../../data/photographers.json')
   const data = await request.json()
 
   return data.photographers
@@ -10,7 +10,7 @@ async function getPhotographers () {
 
 // Retrieve medias data
 async function getMedia () {
-  const request = await fetch('/Front-End-Fisheye/../../data/photographers.json')
+  const request = await fetch('./../../data/photographers.json')
   const { media } = await request.json()
   return media
 }
